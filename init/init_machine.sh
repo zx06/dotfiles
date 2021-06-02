846dfd9547b068e06c180124e54ca7cf6c775daf
zx06 <x_zhuo@live.com>
2021-06-02 17:45:32 +0800

    增加 git镜像

--- init/init_machine.sh
+++ init/init_machine.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
+GITHUB_RAW_MIRROR="https://raw.fastgit.org"
+# GITHUB_RAW_MIRROR="https://raw.githubusercontent.com"
+
 ######################
 # Install oh-my-zsh
 ######################
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL ${GITHUB_RAW_MIRROR}/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "oh-my-zsh installed!"
+
+######################
+# Install pyenv
+######################
+echo "Installing pyenv..."
+sh -c "$(curl -fsSL ${GITHUB_RAW_MIRROR}/pyenv/pyenv-installer/master/bin/pyenv-installer)"
+echo "pyenv installed!"
+
+
+######################
+# Install poetry
+######################
+echo "Installing poetry..."
+sh -c "$(curl -fsSL ${GITHUB_RAW_MIRROR}/python-poetry/poetry/master/get-poetry.py)"
+echo "poetry installed!"
 
 ######################
 # Install gvm
 ######################
 echo "Installing gvm..."
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+sh -c "$(curl -fsSL ${GITHUB_RAW_MIRROR}/moovweb/gvm/master/binscripts/gvm-installer)"
 echo "gvm installed!"
