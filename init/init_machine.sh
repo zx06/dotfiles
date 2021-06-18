a3e4b3eb74b370a987bb4c56b144aa6d3df52ea6
x_zhuo <x_zhuo@live.com>
2021-06-18 12:32:25 +0800

    优化安装脚本

--- init/init_machine.sh
+++ init/init_machine.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 set -e
 
-GITHUB_RAW_MIRROR="https://raw.fastgit.org"
-# GITHUB_RAW_MIRROR="https://raw.githubusercontent.com"
+# GITHUB_RAW_MIRROR="https://raw.fastgit.org"
+GITHUB_RAW_MIRROR="https://raw.githubusercontent.com"
 
+# $1:coler $2:str
+function colored_echo(){
+    echo -e "\033[$1m$2\033[0m"
+}
 
 
 function curl_sh_install(){
     echo "Installing $1..."
     sh -c "$(curl -fsSL ${GITHUB_RAW_MIRROR}/$2)"
-    echo "$1 installed!"
+    colored_echo 32 "$1 installed!"
 }
+
+
+
 ######################
 # Install oh-my-zsh
 ######################
-curl_sh_install oh-my-zsh ohmyzsh/ohmyzsh/master/tools/install.sh
+if test -d ~/.oh-my-zsh
+then
+    colored_echo 33 "oh-my-zsh already installed."
+else
+    curl_sh_install oh-my-zsh ohmyzsh/ohmyzsh/master/tools/install.sh
+fi
 
 
 ######################
 # Install pyenv
 ######################
-curl_sh_install pyenv pyenv/pyenv-installer/master/bin/pyenv-installer
-
+if test -d ~/.pyenv
+then
+    colored_echo 33 "pyenv already installed."
+else
+    curl_sh_install pyenv pyenv/pyenv-installer/master/bin/pyenv-installer
+fi
 
 ######################
 # Install gvm
 ######################
-curl_sh_install gvm moovweb/gvm/master/binscripts/gvm-installer
+if test -d ~/.gvm
+then
+    colored_echo 33 "gvm already installed."
+else
+    curl_sh_install gvm moovweb/gvm/master/binscripts/gvm-installer
+fi
--- zshrc
+++ zshrc
@@ -89,6 +89,8 @@ source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
+source ~/.aliases.sh
+
 # export MANPATH="/usr/local/man:$MANPATH"
 
 # You may need to manually set your language environment
